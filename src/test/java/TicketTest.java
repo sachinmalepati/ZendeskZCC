cda737f027a2060cf8cc7856ad07aa64ee4142af
sachinmalepati <sachin.malepati@gmail.com>
2021-11-28 18:06:44 -0500

    initial junits

--- src/test/java/TicketTest.java
+++ src/test/java/TicketTest.java
@@ -5,5 +5,26 @@ public class TicketTest {
     @Test
     public void constructorTest(){
         Ticket t = new Ticket(JSONObject.NULL,JSONObject.NULL, 1, JSONObject.NULL, JSONObject.NULL, JSONObject.NULL, JSONObject.NULL);
+
+        assert t.id == 1;
+        assert t.priority.equals("");
+        assert t.description.equals("");
+        assert t.type.equals("");
+        assert t.subject.equals("");
+        assert t.status.equals("");
+        assert t.created_at.equals("");
+    }
+
+    @Test
+    public void constructorTest2(){
+        Ticket t = new Ticket("12324","test desc", 2, "prio", "status", "subject", "type");
+
+        assert t.created_at.equals("12324");
+        assert t.description.equals("test desc");
+        assert t.id == 2;
+        assert t.priority.equals("prio");
+        assert t.status.equals("status");
+        assert t.type.equals("type");
+        assert t.subject.equals("subject");
     }
 }
--- src/test/java/TicketsWrapperTest.java
+++ src/test/java/TicketsWrapperTest.java
@@ -10,6 +10,18 @@ public class TicketsWrapperTest {
     public void constructorTest(){
         TicketsWrapper t = new TicketsWrapper(JSONObject.NULL,JSONObject.NULL, 1, new ArrayList<>());
         assert t.count == 1;
+        assert t.next_page.equals("");
+        assert t.previous_page.equals("");
+        assert t.listOfTickets.size()==0;
+    }
+
+    @Test
+    public void constructorTest2(){
+        TicketsWrapper t = new TicketsWrapper("next page","previous page", 1, new ArrayList<>());
+        assert t.count == 1;
+        assert t.next_page.equals("next page");
+        assert t.previous_page.equals("previous page");
+        assert t.listOfTickets.size()==0;
     }
 
 }
